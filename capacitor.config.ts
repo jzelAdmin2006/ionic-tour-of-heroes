0344f4bdc1b4fd0e40760adbed70539f95397f57
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-01-09 11:22:57 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-tour-of-heroes',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
